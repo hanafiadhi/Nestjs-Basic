6955d29fa8cd9db285841997b8c664b97ab53426
hanafi <hanafiadhi890@gmail.com>
2023-10-31 16:00:04 +0700

    firts

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,17 +3,26 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { COFFE_BRANDS } from './coffe.constant';
 
-class ConfigService{}
-class DevelopmentService{}
-class ProductionService{}
+class ConfigService {}
+class DevelopmentService {}
+class ProductionService {}
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService,{
-    provide: COFFE_BRANDS,
-    useValue: ['kapal api','luwak','lampung']
-},{
-    provide: ConfigService,
-    useClass: process.env.NODE_ENV === 'development' ? DevelopmentService : ProductionService
-}] ,
-exports :[CoffeesService]
+@Module({
+  controllers: [CoffeesController],
+  providers: [
+    CoffeesService,
+    {
+      provide: COFFE_BRANDS,
+      useValue: ['kapal api', 'luwak', 'lampung'],
+    },
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentService
+          : ProductionService,
+    },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
